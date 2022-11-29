25ff9d4e6867c341d23840e1ada2cd3e4e107093
SethAllen-ai <77354497+SethAllen-ai@users.noreply.github.com>
2022-11-29 09:37:16 -0600

    Add files via upload

---
+++ BaseBallExample/DisplayTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BaseBallExample
+{
+    public partial class DisplayTable : Form
+    {
+        public DisplayTable()
+        {
+            InitializeComponent();
+        }
+
+        //Entity Framework DB content
+        private BaseBallExample.BaseballEntities dbcontext
+            = new BaseBallExample.BaseballEntities();
+
+        private void DisplayTable_Load(object sender, EventArgs e)
+        {
+            dbcontext.P
+        }
+    }
+}
